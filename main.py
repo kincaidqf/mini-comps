f7fede4c276a7458093f48ef3b24e6e3d85c60a3
Kincaid <kincaidqf@gmail.com>
2025-04-26 11:02:32 -0700

    combined gdp+cpi data into one table

--- main.py
+++ main.py
@@ -19,13 +19,19 @@ inflation_quarterly = inflation.resample('Q').mean()
 # After resampling CPI to quarterly
 inflation_quarterly.index = inflation_quarterly.index + pd.Timedelta(days=1)
 
-print(inflation_quarterly.head())
-inflation_quarterly.to_csv('inflation_quarterly_data.csv')
+# Merge on index (date)
+macro_data = pd.merge(gdp, inflation_quarterly, left_index=True, right_index=True)
+macro_data.columns = ['GDP', 'CPI']
+
+print(macro_data.head())
+macro_data.to_csv('macro_data.csv')
 
 # Display heads
 print(gdp.head())
 print(inflation.head())
+print(inflation_quarterly.head())
 
 # Save locally if you want
 gdp.to_csv('gdp_data.csv')
 inflation.to_csv('inflation_data.csv')
+inflation_quarterly.to_csv('inflation_quarterly_data.csv')
