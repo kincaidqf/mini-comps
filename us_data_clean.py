71487a83776e28e973042cf384c0d370a0b35b2d
Kincaid <kincaidqf@gmail.com>
2025-05-06 14:23:36 -0700

    updating csv column names

--- us_data_clean.py
+++ us_data_clean.py
@@ -21,15 +21,15 @@ gdp.index = gdp.index + pd.Timedelta(days=-1)
 
 # Merge on index (date)
 macro_data = pd.merge(gdp, inflation_quarterly, left_index=True, right_index=True)
-macro_data.columns = ['GDP', 'CPI']
+macro_data.columns = ['US_GDP_pct', 'US_CPI_pct']
 
-macro_data['GDP'] = macro_data['GDP'].pct_change() * 100
-macro_data['CPI'] = macro_data['CPI'].pct_change() * 100
+macro_data['US_GDP_pct'] = macro_data['US_GDP_pct'].pct_change() * 100
+macro_data['US_CPI_pct'] = macro_data['US_CPI_pct'].pct_change() * 100
 
-macro_data['GDP'] = macro_data['GDP'].round(3)
-macro_data['CPI'] = macro_data['CPI'].round(3)
+macro_data['US_GDP_pct'] = macro_data['US_GDP_pct'].round(3)
+macro_data['US_CPI_pct'] = macro_data['US_CPI_pct'].round(3)
 
-macro_data.dropna(subset=['GDP', 'CPI'], inplace=True)
+macro_data.dropna(subset=['US_GDP_pct', 'US_CPI_pct'], inplace=True)
 
 # Save locally
 # gdp.to_csv('gdp_data.csv')
