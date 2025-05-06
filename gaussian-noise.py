54d649e799a2c25ecde9cdc3742d9bb63f24767a
Kincaid <kincaidqf@gmail.com>
2025-05-06 14:34:54 -0700

    combine uk and us datasets

--- gaussian-noise.py
+++ gaussian-noise.py
@@ -4,18 +4,9 @@ import pandas as pd
 # Set the random seed for reproducibility
 np.random.seed(42)
 
-macro_data = pd.read_csv('macro_data.csv', index_col=0, parse_dates=True)
+uk_df = pd.read_csv('data/uk_macro_cleaned.csv')
+us_df = pd.read_csv('data/us_macro_cleaned.csv')
 
-# Define noise parameters
-noise_std_gdp = 0.01 * macro_data['GDP'].std()  # 1% of GDP standard deviation
-noise_std_cpi = 0.01 * macro_data['CPI'].std()  # 1% of CPI standard deviation
+combined_df = pd.concat([uk_df, us_df], ignore_index=True)
 
-# Add Gaussian noise
-macro_data_noisy = macro_data.copy()
-macro_data_noisy['GDP'] += np.random.normal(0, noise_std_gdp, size=len(macro_data))
-macro_data_noisy['CPI'] += np.random.normal(0, noise_std_cpi, size=len(macro_data))
-
-macro_data_noisy.to_csv('macro_data_noisy.csv')
-
-# Now you have the noisy version!
-print(macro_data_noisy.head())
+print(combined_df.head())
